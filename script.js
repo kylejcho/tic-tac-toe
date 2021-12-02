dada1ce39f4dd47b4252d6022e9d8fd28f009f18
Kyle Cho <kylejcho@gmail.com>
2021-12-02 17:53:28 -0500

    Win scenarios

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const gameboard = (() => {
     let boardArr = [[],[],[],[],[],[],[],[],[]];
-    
+
     return {boardArr};
 })();
 
@@ -25,12 +25,47 @@ const gameController = (() => {
         displayController.updateDisplay(gameboard.boardArr)
         switchTurn();
     }
-
+    
     const winScenarios = () => {
         let board = gameboard.boardArr;
-        if (board[0] == 'x' && board[1] == 'x' && board[2] == 'x') {
-            setTimeout(function(){ alert("You Win"); }, 10);
+        if (board[0].length > 0) {
+            diagonal(0, board[0], board);
+            horizontal(0, board[0], board);
+            vertical(0, board[0], board)      
+        } 
+        for (let i = 1; i < 3; i++) {
+            if (board[i].length > 0) {
+                vertical(i, board[i], board)
+            }
         }
+        for (let i = 3; i <= 6; i += 3) {
+            if (board[i].length > 0) {
+                horizontal(i, board[i], board)
+            } 
+        }
+    }
+
+    const horizontal = (index, sign, board) => {
+        if (board[index+1] == sign && board[index+2] == sign) {
+            win();
+        }
+    }
+    const vertical = (index, sign, board) => {
+        if (board[index+3] == sign && board[index+6] == sign) {
+            win();
+        }
+    }
+    const diagonal = (index, sign, board) => {
+        if (index == 0 && board[4] == sign && board[8] == sign) {
+            win();
+        } 
+        else if (index == 2 && board[4] == sign && board[6] == sign) {
+            win();
+        }
+    }
+    
+    const win = () => {
+        setTimeout(function(){ alert("You Win"); }, 10);
     }
 
     return {mark, winScenarios}
@@ -57,3 +92,4 @@ const markClick = (() => {
     })
 })();
 
+//setTimeout(function(){ alert("You Win"); }, 10);
