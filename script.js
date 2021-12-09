1379718f5e770d642084fc23e68d647566ff0302
Kyle Cho <kylejcho@gmail.com>
2021-12-09 17:39:39 -0500

    Computer difficulty modes

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let boardArr = [[],[],[],[],[],[],[],[],[]];
 const game = (() => {
     let winState = false;
     let gameMode = "computer";
+    let botDifficulty = "easy";
     let currentTurn = 'x';
 
 
@@ -12,11 +13,28 @@ const game = (() => {
             gameMode = ""
             restart()
             playerTurn();
+        } else if (selection.value == "Easy"){
+            gameMode = "computer";
+            botDifficulty = "easy";
+            restart();
+            playerTurn();
+        } else if (selection.value == "Medium"){
+            gameMode = "computer";
+            botDifficulty = "medium";
+            restart();
+            playerTurn();
+        } else if (selection.value == "Hard"){
+            gameMode = "computer";
+            botDifficulty = "hard";
+            restart();
+            playerTurn();
         } else {
             gameMode = "computer";
+            botDifficulty = "impossible";
             restart();
             playerTurn();
         }
+        console.log(botDifficulty)
     }
 
     const getGameMode = () => gameMode;
@@ -27,20 +45,24 @@ const game = (() => {
         if (currentTurn == 'x') currentTurn = 'o';
         else currentTurn = 'x';
         playerTurn();
+
+        if (gameMode == "computer" && currentTurn == "o" && winState == false) {
+            computer.difficulty(botDifficulty);
+        } 
     }
 
     const mark = (index) => {
         audio.pop()
         boardArr[index] = currentTurn;
         display.update(boardArr);
+        winCheck(boardArr);
+        tieCheck();
         switchTurn();
-        
-        if (gameMode == "computer" && currentTurn == "o") computer.bestMove(); 
     }
 
     const tieCheck = () => {
-        let a = boardArr.every(i => i.length > 0 && winState == false);
-        if (a) display.tie();
+        let a = boardArr.every(i => i.length > 0);
+        if (a == true && winState == false) display.tie();
     }
     
     const winCheck = (board) => {
@@ -66,64 +88,35 @@ const game = (() => {
 
     const horizontal = (i, sign, board) => {
         if (board[i+1] == sign && board[i+2] == sign) {
-            let x;
-            if (game.getGameMode() == "computer") {
-                x = 800
-            } else {
-                x = 0
-            }
             document.querySelector('#unclickableDiv').style.visibility = "visible";
-            setTimeout(function(){
-                display.threeRow(i, i + 1, i + 2);
-                gameEnd(sign);
-            }, x);
-            
+            display.threeRow(i, i + 1, i + 2);
+            gameEnd(sign);
         }
     }
     const vertical = (i, sign, board) => {
-        let x;
-        if (game.getGameMode() == "computer") {
-            x = 800
-        } else {
-            x = 0
-        }
         if (board[i+3] == sign && board[i+6] == sign) {
             document.querySelector('#unclickableDiv').style.visibility = "visible";
-            setTimeout(function(){
-                display.threeRow(i, i + 3, i + 6); 
-                gameEnd(sign);
-            }, x);
-
-
+            display.threeRow(i, i + 3, i + 6); 
+            gameEnd(sign);
         }
     }
     const diagonal = (i, sign, board) => {
-        let x;
-        if (game.getGameMode() == "computer") {
-            x = 800
-        } else {
-            x = 0
-        }
         if (i == 0 && board[4] == sign && board[8] == sign) {
             document.querySelector('#unclickableDiv').style.visibility = "visible";
-            setTimeout(function(){
-                display.threeRow(0, 4, 8);
-                gameEnd(sign);
-            }, x);
+            display.threeRow(0, 4, 8);
+            gameEnd(sign);
+
             
         } else if (i == 2 && board[4] == sign && board[6] == sign) {
             document.querySelector('#unclickableDiv').style.visibility = "visible";
-            setTimeout(function(){
-                display.threeRow(2, 4, 6);
-                gameEnd(sign);
-            }, x);
-            
+            display.threeRow(2, 4, 6);
+            gameEnd(sign);
         }
     }
     
     const gameEnd = (sign) => {
         display.gameEndPopup(sign);
-        return winState = true;
+        winState = true;
     }
 
     const clearBoardArr = () => boardArr = [[],[],[],[],[],[],[],[],[]];
@@ -254,8 +247,8 @@ document.addEventListener('click', (e) => {
     if (e.target.classList == 'space' && e.target.innerHTML.length == 0) {
         game.mark(e.target.id[1]);
     }
-    game.winCheck(boardArr);
-    game.tieCheck();
+    //game.winCheck(boardArr);
+    //game.tieCheck();
 })
 
 const selection = document.querySelector('select');
@@ -278,12 +271,48 @@ const computer = (() => {
     let tieState = false;
     let testSign = ''
 
+
     let scores = {"x": -1, 'o': 1,'tie': 0};
-    
-    const botDifficulty = (difficulty) => {
+
+
+    const difficulty = (difficulty) => {
+        let botDifficulty
         if (difficulty == "easy") {
-            
+            return randomMove();
+        } else if (difficulty == "medium") {
+            botDifficulty = Math.floor(Math.random() * 2)
+        } else if (difficulty == "hard") {
+            botDifficulty = Math.floor(Math.random() * 3)
+        } else if (difficulty == "impossible") {
+            return bestMove();
+        } 
+        if (botDifficulty == 0 ) {
+            randomMove();
+        } else {
+            bestMove();
         }
+    }
+
+    const randomMove = () => {
+        if (document.querySelector('#unclickableDiv').style.visibility != "visible") {
+            let i = Math.floor(Math.random() * 9);
+            if (boardArr[i] == '') {
+                boardArr[i] = 'o';
+                game.winCheck(boardArr);
+                game.tieCheck();
+            
+                setTimeout(function(){
+                    if (document.querySelector('#unclickableDiv').style.visibility != "visible") {
+                        audio.pop();
+                    }
+                    display.update(boardArr); 
+                    game.switchTurn();
+                }, 650);
+            } else {
+                randomMove();
+            }
+        }
+        
     }
 
     const bestMove = () => {
@@ -293,6 +322,9 @@ const computer = (() => {
         for (let i = 0; i < 9; i++) {
             if (testBoard[i] == '') {
                 testBoard[i] = 'o';
+                game.winCheck(boardArr);
+                game.tieCheck();
+
                 let score = minimax(testBoard, 0, false);
                 testBoard[i] = '';
                 if (score > bestScore) {
@@ -400,5 +432,5 @@ const computer = (() => {
             testSign = sign
         }
     }
-    return {bestMove}
+    return {bestMove, difficulty}
 })();
